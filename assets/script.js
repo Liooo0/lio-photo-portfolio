53a765f3fd35ec1dd42af77c863db64749d96533
Liooo0 <114926712+Liooo0@users.noreply.github.com>
2025-09-02 17:43:56 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
+// 自动加载 /images/ 目录下的图片
+const gallery = document.querySelector(".gallery-grid");
+
+const images = [
+  "photo1.jpg",
+  "photo2.jpg",
+  "photo3.jpg",
+  "photo4.jpg",
+];
+
+images.forEach(file => {
+  const div = document.createElement("div");
+  div.className = "gallery-item";
+  div.innerHTML = `<img src="images/${file}" alt="作品">`;
+  gallery.appendChild(div);
+});
 
