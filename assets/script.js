4726e691f7ed2b5ac8317ee157da5ca5aa683734
Liooo0 <114926712+Liooo0@users.noreply.github.com>
2025-09-02 18:16:09 +0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,15 @@
-// 自动加载 /images/ 目录下的图片
-const gallery = document.querySelector(".gallery-grid");
+const gallery = document.getElementById("gallery-grid");
 
+// 这里写相对路径（相对于 index.html）
 const images = [
-  "photo1.jpg",
-  "photo2.jpg",
-  "photo3.jpg",
-  "photo4.jpg",
+  "images/star/star01.jpg",
+  "images/star/star02.jpg",
+  "images/star/star03.jpg",
 ];
 
 images.forEach(file => {
-  const div = document.createElement("div");
-  div.className = "gallery-item";
-  div.innerHTML = `<img src="images/${file}" alt="作品">`;
-  gallery.appendChild(div);
+  const img = document.createElement("img");
+  img.src = file;
+  img.alt = "摄影作品";
+  gallery.appendChild(img);
 });
-
